708d492335df0ef1965409144b7865ecb003648e
kngssd <danakengessova@gmail.com>
2024-08-08 15:25:59 +0100

    update if yes statements, names of variables (60%)

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -3,25 +3,40 @@ public class TennisGame3 implements TennisGame {
     
     private int player2Points;
     private int player1Points;
-    private String p1N;
-    private String p2N;
+    private String player1Name;
+    private String player2Name;
+    private static String[] PointsNames = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
 
-    public TennisGame3(String p1N, String p2N) {
-        this.player1Name = p1N;
-        this.player2Name = p2N;
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
-        String s;
-        if (player1Points < 4 && player2Points < 4 && !(player1Points + player2Points == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[player1Points];
-            return (player1Points == player2Points) ? s + "-All" : s + "-" + p[player2Points];
+        boolean isGameOver = !((player1Points < 4 && player2Points < 4) && !(player1Points + player2Points == 6));
+        if (isGameOver) {
+            String score = PointsNames[player1Points];
+            if (player1Points == player2Points) {
+                return score + "-All";
+            } else {
+               return score + "-" + PointsNames[player2Points];
+            }
         } else {
             if (player1Points == player2Points)
                 return "Deuce";
-            s = player1Points > player2Points ? p1N : p2N;
-            return ((player1Points-player2Points)*(player1Points-player2Points) == 1) ? "Advantage " + s : "Win for " + s;
+
+            String leader;
+            if (player1Points > player2Points) {
+               leader = player1Name;
+            } else {
+                leader = player2Name;
+            }
+        }
+            boolean isAdvantage = (player1Points-player2Points)*(player1Points-player2Points) == 1;
+            if (isAdvantage) {
+                return "Advantage " + leader;
+            } else {
+            return "Win for " + leader;
         }
     }
     
