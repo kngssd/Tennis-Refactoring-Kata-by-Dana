4eafeea3e465f6a0e1984a4f8f879ab33ec2b8b3
kngssd <danakengessova@gmail.com>
2024-08-08 15:58:04 +0100

    added some extractions inside the tieScore function. still need to work on it

--- java/src/main/java/TennisGame6.java
+++ java/src/main/java/TennisGame6.java
@@ -18,69 +18,61 @@ public class TennisGame6 implements TennisGame {
 
     }
 
-    public String getScore()
-    {
-        String result;
+    // tie score
+    private String tieScore() {
+        return switch (player1Score) {
+            case 0 -> "Love-All";
+            case 1 -> "Fifteen-All";
+            case 2 -> "Thirty-All";
+            default -> "Deuce";
+        };
+    }
 
-        if (player1Score == player2Score)
+    // end-game score
+    private String endGameScore() {
+        int scoreDifference = player1Score - player2Score;
+        if (scoreDifference == 1) {
+            return "Advantage " + player1Name;
+        } else if (scoreDifference == -1) {
+            return "Advantage " + player2Name;
+        } else if (scoreDifference >= 2) {
+            return "Win for " + player1Name;
+        } else {
+            return "Win for " + player2Name;
+        }
+    }
+
+
+    public String getScore(){
+        if (player1Score == player2Score) {
+            return tieScore();
+        } else if (player1Score >= 4 || player2Score >= 4) {
+
+        }
+    }
+
+
         {
-            // tie score
-            String tieScore;
-            switch (player1Score)
-            {
-                case 0:
-                    tieScore = "Love-All";
-                    break;
-                case 1:
-                    tieScore = "Fifteen-All";
-                    break;
-                case 2:
-                    tieScore = "Thirty-All";
-                    break;
-                default:
-                    tieScore = "Deuce";
-                    break;
-            }
+
+
 
             result = tieScore;
         }
-        else if (player1Score >= 4 || player2Score >= 4)
+        else if
         {
-            // end-game score
-            String endGameScore;
 
-            if (player1Score - player2Score == 1) {
-                endGameScore = "Advantage " + player1Name;
-            } else if (player1Score - player2Score == -1) {
-                endGameScore = "Advantage " + player2Name;
-            } else if (player1Score - player2Score >= 2) {
-                endGameScore = "Win for " + player1Name;
-            } else {
-                endGameScore = "Win for " + player2Name;
-            }
-
-            result = endGameScore;
         }
         else
         {
             // regular score
-            String regularScore;
-
-            String score1 =  switch (player1Score)
-            {
-                case 0 -> "Love";
-                case 1 -> "Fifteen";
-                case 2 -> "Thirty";
-                default -> "Forty";
-            };
-
-            var score2 =  switch (player2Score)
-            {
-                case 0 -> "Love";
-                case 1 -> "Fifteen";
-                case 2 -> "Thirty";
-                default -> "Forty";
-            };
+            private String regularScoreDescription(int score) {
+                return switch (score) {
+                    case 0 -> "Love";
+                    case 1 -> "Fifteen";
+                    case 2 -> "Thirty";
+                    default -> "Deuce";
+                };
+        }
 
             regularScore = score1 + "-" + score2;
 
